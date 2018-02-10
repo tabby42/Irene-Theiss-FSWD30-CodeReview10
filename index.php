dec7448c74925de9ef85298571412cc9527ddf53
tabby42 <irene.theiss@gmx.at>
2018-02-10 13:10:41 +0100

    add redirection to login if username is not set

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php include "partials/header.php";?>
 <?php include "lib/Media.php";?>
+<?php if (!isset($_SESSION['username'])) : ?>
+<?php     header("location: login.php");?>
+<?php endif ?>
 
-<header id="header" class="pad-row ">
+<!-- <header id="header" class="pad-row ">
   <h1 class="text-center text-uppercase">Anyone who says<br>they have only one life to live<br>must not know<br>how to read a book.<span><small>&mdash; Author Unknown &mdash;</small></span>
   </h1>
-</header><!-- /header -->
+</header> -->
+<!-- /header -->
 <div>
 	<?php $media = Media::getAllMedia(); 
 			//var_dump($media);?>
