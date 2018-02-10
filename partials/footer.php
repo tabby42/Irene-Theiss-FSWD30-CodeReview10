e6352fbdec5a57b292bb747dfd1f89c9aea17f75
tabby42 <irene.theiss@gmx.at>
2018-02-10 17:05:26 +0100

    update footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,7 +1,4 @@
 	<footer class="text-center dark-bg">
-      <ul class="list-inline list-unstyled" style="padding-top:20px;">
-        <li>&copy; 2018 Big Library</li>
-      </ul>
     </footer><!-- /footer -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
