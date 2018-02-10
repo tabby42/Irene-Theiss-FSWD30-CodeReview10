303f6ce4e7d3a7eec4565d12a5a650350e2e4026
tabby42 <irene.theiss@gmx.at>
2018-02-10 14:39:55 +0100

    add display of media details

--- show-details.php
+++ show-details.php
@@ -1,21 +1,27 @@
 <?php include "partials/header.php";?>
-
-<?php
-	include "lib/Media.php";
-	if($_SERVER["REQUEST_METHOD"] == "GET"){
-		if (isset($_GET["mediaId"])) {
-			$id = $_GET["mediaId"];
-			//var_dump($id);
-			$media = new Media();
-			$media->mediaId = $id;
-			if ($media->getMediaById()) {
-				$media = $media->getMediaById();
-				//echo $media->title;
+<div class="container-fluid">
+  <div class="row dark-bg  media-container detail-container">
+  	<div class="col-md-2 hidden-xs">
+  	</div>
+	<?php
+		include "lib/Media.php";
+		if($_SERVER["REQUEST_METHOD"] == "GET"){
+			if (isset($_GET["mediaId"])) {
+				$id = $_GET["mediaId"];
+				//var_dump($id);
+				$media = new Media();
+				$media->mediaId = $id;
+				if ($media->getMediaById()) {
+					$media = $media->getMediaById();
+					$media->displayMediaDetails();
+				}
+			} else {
+				echo "Media could not be found.";
 			}
-		} else {
-			echo "Media could not be found.";
 		}
-	}
-?>
-
+	?>
+	<div class="col-md-2 hidden-xs">
+  	</div>
+  </div>
+</div><!-- /.container-fluid -->
 <?php include "partials/footer.php";?>
